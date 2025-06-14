6c95e63ae7ccd082d8faeda8f102345a52aef784
Abdvijay <abdvijay19@gmail.com>
2025-06-14 12:54:22 +0530

    Mail changes updated

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -14,13 +14,10 @@
 
       let thisForm = this;
 
-      let action = thisForm.getAttribute('action');
+      let action = thisForm.getAttribute('action')  || '/';;
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
       
-      if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!');
-        return;
-      }
+
       thisForm.querySelector('.loading').classList.add('d-block');
       thisForm.querySelector('.error-message').classList.remove('d-block');
       thisForm.querySelector('.sent-message').classList.remove('d-block');
